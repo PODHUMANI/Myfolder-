c6e13f372201732a66eed50787009de127beb6a1
PODHUMANI M <156355216+PODHUMANI@users.noreply.github.com>
2024-02-10 21:05:46 +0530

    Create Pro1.java

---
+++ Pro1.java
@@ -0,0 +1,19 @@
+public class pro1{
+    public static void main(String[]args){
+        int arr[]={23,34,15,51,65};
+        int first=0;
+        int second=0;
+        for(int i=0;i<5;i++){
+            if(arr[i]>first){
+                first=arr[i];
+            }
+  }
+  for(int i=0;i<5;i++){
+    if(arr[i]<first && arr[i]>second){
+        second=arr[i];
+    }
+
+
+}  System.out.println(second);
+    }
+}
