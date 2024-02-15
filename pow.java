89f2fa1b90efb3a0ee05f6375865a9a37b90936b
PODHUMANI M <156355216+PODHUMANI@users.noreply.github.com>
2024-02-15 08:53:17 -0800

    Create pow.java

---
+++ pow.java
@@ -0,0 +1,9 @@
+public class pow {
+    public static void main(String[] args) {
+        int x=13;
+        int y=4;
+      int las=x%10;
+      final double pow=Math.pow(las,y);
+      System.out.println((int)pow);
+    }
+}
