71d4836c90a014404641369a242f39e9537a30cf
PODHUMANI M <156355216+PODHUMANI@users.noreply.github.com>
2024-09-10 08:23:09 +0530

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,12 @@
+class Solution
+{
+public int[] sumto( int[] num , int target){
+map <Integer, Integer> map = new MashMap<>(); 
+for(i=0;i< num;i++){
+if(map.costainkey(target - num[i]))
+return new int[]{i,map.get(target-num[i])}
+map.put(number[i],i);
+}
+retare null;
+}
+}
