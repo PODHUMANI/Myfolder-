a4bae378b77022168b44a97e3e1f3ced2c1da357
PODHUMANI M <156355216+PODHUMANI@users.noreply.github.com>
2024-09-24 14:33:15 +0530

    Create reverseString.java

---
+++ reverseString.java
@@ -0,0 +1,5 @@
+public class reverseString{
+public static void main (String[]){ 
+String str= "automatic";
+StringBuilder reversed =new StringBuilder(str). reverse ();
+System.out.println(reversed);}}
